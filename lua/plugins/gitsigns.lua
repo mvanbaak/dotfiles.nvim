7e89b5ae38412daf0f4a0da9fc2c97d9618315e4
Michiel van Baak Jansen <michiel@vanbaak.eu>
2025-11-10 00:57:10 +0100

    Add gitsigns

---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,11 @@
+-- ================================================================================================
+-- TITLE : gitsigns.nvim
+-- ABOUT : deep buffer integration for git.
+-- LINKS :
+--   > github : https://github.com/lewis6991/gitsigns.nvim
+-- ================================================================================================
+
+return {
+	"lewis6991/gitsigns.nvim",
+	opts = {},
+}
