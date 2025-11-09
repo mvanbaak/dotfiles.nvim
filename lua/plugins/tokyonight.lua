3dcd9e9ee78a28cddffe32ba892e549058e6c0ad
Michiel van Baak Jansen <michiel@vanbaak.eu>
2025-11-09 23:56:22 +0100

    Add tokyonight theme

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,15 @@
+-- ================================================================================================
+-- TITLE : folke/tokyonight.nvim
+-- ABOUT : A clean, dark Neovim theme
+-- LINKS :
+--   > github : https://github.com/folke/tokyonight.nvim
+-- ================================================================================================
+
+return {
+    "folke/tokyonight.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+        vim.cmd([[colorscheme tokyonight]])
+    end,
+}
