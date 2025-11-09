18f8c62e62896012f0013d7749940beed6c6ecdb
Michiel van Baak Jansen <michiel@vanbaak.eu>
2025-11-10 00:56:00 +0100

    Set correct theme for lazy

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -41,7 +41,7 @@ require("lazy").setup({
     },
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
-    install = { colorscheme = { "habamax" } },
+    install = { colorscheme = { "tokyonight" } },
     -- configure look/feel
     ui = { border = "rounded" },
     -- automatically check for plugin updates
