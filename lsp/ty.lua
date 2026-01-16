f2d1390aa87e27501c4bc7efed8a2c1f45d295ac
Michiel van Baak Jansen <michiel@vanbaak.eu>
2026-01-16 08:37:13 -0300

    Add missing file to cowfigure ty lsp

---
+++ lsp/ty.lua
@@ -0,0 +1,8 @@
+-- ================================================================================================
+-- TITLE : ty
+-- ABOUT : An extremely fast Python type checker and language server, written in Rust.
+-- LINKS :
+--   > github : https://github.com/astral-sh/ty
+-- ================================================================================================
+
+return {}
