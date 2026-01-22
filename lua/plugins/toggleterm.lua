904fda178d7d91d4b6795866d8f378b55dd086ac
Michiel van Baak Jansen <michiel@vanbaak.eu>
2026-01-22 13:03:38 -0300

    Add toggleterm (toggle with <leader>tt) plugin

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,18 @@
+return {
+    'akinsho/toggleterm.nvim',
+    config = function()
+        require("toggleterm").setup({
+            size = 10,
+            open_mapping = [[<leader>tt]],
+            shading_factor = 2,
+            direction = "float",
+            float_opts = {
+                border = "curved",
+                highlights = {
+                    border = "Normal",
+                    background = "Normal",
+                },
+            },
+        })
+    end,
+}
