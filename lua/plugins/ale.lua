e3d14bfbb947852548558292ec810c9a32a897db
Michiel van Baak Jansen <michiel@vanbaak.eu>
2026-01-23 07:55:20 -0300

    Enable ale fixers on save again

--- lua/plugins/ale.lua
+++ lua/plugins/ale.lua
@@ -44,7 +44,7 @@ return {
             -- typescriptreact = js_fixers,
         }
         -- fix on save
-        g.ale_fix_on_save = 0
+        g.ale_fix_on_save = 1
         -- disable lsp stuff
         g.ale_disable_lsp = 1
         -- Report using neovim diagnostics
