cb0572b801810dc201f5d1640a307bd8288f169c
Michiel van Baak Jansen <michiel@vanbaak.eu>
2026-05-20 13:47:09 +0200

    Disable netrw in preperation for one of the nvim-tree or neo-tree plugins

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -114,3 +114,7 @@ vim.g.loaded_node_provider = 0 -- Disable node.js support
 vim.g.loaded_perl_provider = 0 -- Disable pers support
 vim.g.loaded_python3_provider = 0 -- Disable python3 support
 vim.g.loaded_ruby_provider = 0 -- Disable ruby support
+
+-- Disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
