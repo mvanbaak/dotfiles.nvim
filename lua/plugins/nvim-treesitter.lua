de917b16a6315e8e5f3cb0ab425d746807664010
Michiel van Baak Jansen <michiel@vanbaak.eu>
2026-05-20 13:53:10 +0200

    feat(treesitter): migrate to neovim 0.12 compatible version
    - Switch from master branch to main (rewrite)
    - Add treesitter-parser-registry dependency
    - Update require path from nvim-treesitter.configs to nvim-treesitter
    - Remove deprecated config options
    
    BREAKING CHANGE: Requires tree-sitter CLI >= 0.26.1 to compile parsers

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,54 +3,24 @@
 -- ABOUT : Treesitter configurations and abstraction layer for Neovim.
 -- LINKS :
 --   > github : https://github.com/nvim-treesitter/nvim-treesitter
+-- NOTE : For Neovim 0.12+, uses the new main branch (rewrite)
 -- ================================================================================================
 
 return {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
+    dependencies = {
+        "neovim-treesitter/treesitter-parser-registry",
+    },
     lazy = false,
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter.configs").setup({
-            -- A list of parser names that MUST always be installed
-            ensure_installed = {
-                "bash",
-                "c",
-                "dockerfile",
-                "javascript",
-                "lua",
-                "markdown_inline",
-                "markdown",
-                "python",
-                "query",
-                "terraform",
-                "tsx",
-                "typescript",
-                "vim",
-                "vimdoc",
-                "yaml",
-            },
-            sync_install = false, -- Install parsers synchronously
-            auto_install = true, -- Automatically install missing parsers when entering buffer
-            -- modules
+        require("nvim-treesitter").setup({
             highlight = {
                 enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            incremental_selection = {
-                enbale = true,
-                keymaps = {
-                    init_selection = "<CR>",
-                    node_incremental = "<CR>",
-                    scope_incremental = "<TAB>",
-                    node_decremental = "<S-TAB>",
-                },
             },
             indent = {
                 enable = true,
             },
-            -- end of modules
-
         })
     end,
 }
