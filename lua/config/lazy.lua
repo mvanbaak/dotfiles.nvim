002c5bd64e941a25718eefd8476549c4904626cb
Michiel van Baak Jansen <michiel@vanbaak.eu>
2026-05-29 11:56:22 +0200

    Remove colorscheme neon that was not used except for lazy

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -37,7 +37,7 @@ require("lazy").setup({
     },
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
-    install = { colorscheme = { "neon" } },
+    install = { colorscheme = { "moonfly" } },
     -- configure look/feel
     ui = { border = "rounded" },
     -- automatically check for plugin updates
